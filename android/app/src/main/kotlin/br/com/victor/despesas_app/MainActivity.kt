a8b9d07bbf2d5e18fc71b978959b1d53af57132b
vctrr1 <victoremaanoel@outlook.com>
2022-02-14 20:18:44 -0300

    Criação inicial do Projeto DespesasApp

---
+++ android/app/src/main/kotlin/br/com/victor/despesas_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.victor.despesas_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(const ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Despesas")),
+        body: const Center(child: Text("Verção 1.1")));
+  }
+}
